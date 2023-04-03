4274215d0daac3c694efd7dc778a76f283fb04d2
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-04-03 20:53:45 +0530

    authReducer, registerActn, loadActn, set authToken

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+/* if token is present it will set it in headers 
+ if not it will delete it from headers. */
+
+const setAuthToken = (token) => {
+    if(token){
+        axios.defaults.headers.common['x.auth.token'] = token;
+    } else{
+        delete axios.defaults.headers.common['x.auth.token'];
+    }
+}
+
+export default setAuthToken;
