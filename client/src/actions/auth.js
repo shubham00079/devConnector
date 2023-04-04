aad2acd165c025a5d3899cd44bba1b78680395af
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-04-04 19:10:59 +0530

    LoginUser

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -61,30 +61,31 @@ export const register =
   };
 
 // Login User
-export const login = (email, password) => async (dispatch) => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  const body = JSON.stringify({ email, password });
-  try {
-    const res = await axios.post('/api/auth', body, config);
+export const login =
+  ({ email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const body = JSON.stringify({ email, password });
+    try {
+      const res = await axios.post('/api/auth', body, config);
 
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data,
-    });
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+      dispatch({
+        type: LOGIN_FAIL,
+      });
     }
-    dispatch({
-      type: LOGIN_FAIL,
-    });
-  }
-};
+  };
