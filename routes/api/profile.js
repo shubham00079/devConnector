e5135f59bffd79f5dbbf7b14f6b37c66d15a9930
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-04-14 15:09:17 +0530

    Error in this file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -74,9 +74,9 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map((skill) => skill.trim());
-    }
+    // if (skills) {
+    //   profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    // }
 
     // Build Social object
     profileFields.social = {};
