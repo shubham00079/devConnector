ae9835133e761e3846e4f028f508e718d8c407d0
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-04-15 13:34:54 +0530

    Delete Action& Reducers

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const Profile = require('../../models/Profile');
 const request = require('request');
 const config = require('config');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { check, validationResult } = require('express-validator');
 
 // @route       GET api/profile/me
@@ -154,7 +155,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access      Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users post
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
