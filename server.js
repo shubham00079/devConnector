15635835d8cf508d51a71618c5cae71e3fdfe976
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-07-28 11:01:21 +0530

    deployment changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,10 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware, in order to use req.body in middlewares we need to pass this.
-app.use(express.json({ extended:false }));
-
-app.get('/', (req,res) => res.send('API Running'));
-
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,6 +16,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (ProgressEvent.env.NODE_ENV == 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
